d590dd7bc4335f6ee958177f3528796b41a296b6
Sunnahboy <126472302+Sunnahboy@users.noreply.github.com>
2026-07-18 22:47:29 +0800

    Simple pytest for the models chunking

---
+++ tests/pipeline/test_chunk_models.py
@@ -0,0 +1,9 @@
+from app.pipeline.chunking.models import Chunk
+
+
+def test_chunk_creation():
+    chuck = Chunk(Chunk_id="1", text="Hello", start_char=0, end_char=5)
+
+    assert chuck.text == "Hello"
+    assert chuck.start_char == 0
+    assert chuck.end_char == 5
