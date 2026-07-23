0e2f69b1a28c8a4e919d4cef3d9a02b36c3a830c
Sunnahboy <126472302+Sunnahboy@users.noreply.github.com>
2026-07-24 04:21:35 +0800

    add Ollama for now

---
+++ app/pipeline/embeddings/factory.py
@@ -0,0 +1,15 @@
+from app.config import settings
+
+from .base import BaseEmbedder
+from .ollama_emdedder import OllamaEmbedder
+
+
+def create_embedder() -> BaseEmbedder:
+    """
+    Create the configured embedding provider.
+     -for now one provider
+    """
+
+    if settings.embedding_model == "ollama":
+        return OllamaEmbedder()
+    raise ValueError(f"unsupported  embedding provider: {settings.embedding_model}")
